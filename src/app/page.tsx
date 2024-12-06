cec7628cc839b7178b80e1eec537e8131dd64086
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-12-06 17:21:27 +0100

    feat: print table with forms & submission count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
+import { Forms } from "~/database";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const forms = await Forms.getAll();
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ol>
-          <li>hello world</li>
-        </ol>
+        <h2>Forms</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Submissions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {forms.map((form) => (
+              <tr key={form.id}>
+                <td>{form.id}</td>
+                <td>{form.title}</td>
+                <td>{form.submissionCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </main>
     </div>
   );
--- src/database/repositories/forms.ts
+++ src/database/repositories/forms.ts
@@ -1,4 +1,5 @@
-import { db, Form, FormData } from '~/database';
+import { db, Form, FormData, Submission } from '~/database';
+import { count, eq } from 'drizzle-orm';
 
 const add = async (data: FormData) => {
   const [result] = await db.insert(Form).values(data);
@@ -6,6 +7,23 @@ const add = async (data: FormData) => {
   return result.insertId;
 };
 
+const getAll = async () => {
+  const forms = await db
+    .select({
+      form: Form,
+      submissionCount: count(Submission.id),
+    })
+    .from(Form)
+    .leftJoin(Submission, eq(Form.id, Submission.formId))
+    .groupBy(Form.id);
+
+  return forms.map((form) => ({
+    ...form.form,
+    submissionCount: form.submissionCount || 0,
+  }));
+};
+
 export const Forms = {
   add,
+  getAll,
 };
